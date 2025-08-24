6efe1fb8bdf4164116d0f793d07e678f906511ae
sainathkokkula <153606718+sainathkokkula@users.noreply.github.com>
2025-08-24 22:42:43 +0530

    Update nexus.sh

--- nexus.sh
+++ nexus.sh
@@ -1,14 +1,28 @@
 sudo yum update -y
 sudo yum install wget -y
 sudo yum install java-17-amazon-corretto-jmods -y
+
+# Create app directory
 sudo mkdir /app && cd /app
-sudo wget https://download.sonatype.com/nexus/3/nexus-3.79.1-04-linux-x86_64.tar.gz
-sudo tar -xvf nexus-3.79.1-04-linux-x86_64.tar.gz
-sudo mv nexus-3.79.1-04 nexus
+
+# Download Nexus 3.83.0-08
+sudo wget https://download.sonatype.com/nexus/3/nexus-3.83.0-08-linux-x86_64.tar.gz
+
+# Extract the tar file
+sudo tar -xvf nexus-3.83.0-08-linux-x86_64.tar.gz
+
+# Rename extracted folder to nexus
+sudo mv nexus-3.83.0-08 nexus
+
+# Create nexus user and set permissions
 sudo adduser nexus
 sudo chown -R nexus:nexus /app/nexus
 sudo chown -R nexus:nexus /app/sonatype*
-sudo sed -i '27  run_as_user="nexus"' /app/nexus/bin/nexus
+
+# Update nexus run_as_user
+sudo sed -i '27 s/^#run_as_user.*/run_as_user="nexus"/' /app/nexus/bin/nexus
+
+# Create systemd service file
 sudo tee /etc/systemd/system/nexus.service > /dev/null << EOL
 [Unit]
 Description=nexus service
@@ -21,13 +35,14 @@ User=nexus
 Group=nexus
 ExecStart=/app/nexus/bin/nexus start
 ExecStop=/app/nexus/bin/nexus stop
-User=nexus
 Restart=on-abort
 
 [Install]
 WantedBy=multi-user.target
 EOL
-sudo chkconfig nexus on
-sudo systemctl start nexus
+
+# Enable and start Nexus service
+sudo systemctl daemon-reload
 sudo systemctl enable nexus
+sudo systemctl start nexus
 sudo systemctl status nexus
